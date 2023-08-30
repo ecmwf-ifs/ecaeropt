b8acc2d905080967cd1913bcc81ddbc28b91bdeb
Ramiro Checa-Garcia <ramiro.checa-garcia@ecmwf.int>
2023-08-30 12:54:42 +0000

    Preparing for github

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,23 +1,26 @@
-####################################################################################
-#  docs/source/conf.py
-#
-#
-#   (C) Copyright 2022- ECMWF.
-#  
-#   This software is licensed under the terms of the Apache Licence Version 2.0
-#   which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
-# 
-#   In applying this licence, ECMWF does not waive the privileges and immunities
-#   granted to it by virtue of its status as an intergovernmental organisation
-#   nor does it submit to any jurisdiction.
-#
-#  Author:
-#     Ramiro Checa-Garcia. ECMWF
-# 
-#  Modifications:
-#     10-Dec-2022   Ramiro Checa-Garcia    1st. version
-#
-###################################################################################
+
+
+#  +------------------------------------------------------------------------------------+
+#  | docs/source/conf.py                                                                |
+#  |                                                                                    |
+#  | (C) Copyright 2022- ECMWF.                                                         |
+#  |                                                                                    |
+#  | This software is licensed under the terms of the Apache Licence Version 2.0        |
+#  | which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.               |
+#  |                                                                                    |
+#  | In applying this licence, ECMWF does not waive the privileges and immunities       |
+#  | granted to it by virtue of its status as an intergovernmental organisation         |
+#  | nor does it submit to any jurisdiction.                                            |
+#  |                                                                                    |
+#  | Author:                                                                            |
+#  |    Ramiro Checa-Garcia. ECMWF                                                      |
+#  |                                                                                    |
+#  | Modifications:                                                                     |
+#  |    10-Dec-2022   Ramiro Checa-Garcia    1st version                                |
+#  |                                                                                    |
+#  | Info:                                                                              |
+#  |                                                                                    |
+#  +------------------------------------------------------------------------------------+
 
 
 # Configuration file for the Sphinx documentation builder.
@@ -29,10 +32,10 @@ import sys
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'py-ecaeropt'
-copyright = '2022/2023, ECMWF'
+project = 'ecaeropt'
+copyright = '2022- ECMWF. (Under Apache License Version 2.0)'
 author = 'Ramiro Checa-Garcia'
-release = 'beta'
+release = '1.16'
 master_doc = 'index'
 source_suffix = '.rst'
 sys.path.append("/home/parc/_ecmwf/codes/ecaeropt/")
@@ -74,6 +77,6 @@ latex_logo = "ECMWF_logo.png"
 latex_elements = {
         'atendofbody': 'ECWMF',
         'papersize': 'a4paper',
-        'releasename':" v1.0 ECMWF",
+        'releasename':" v1.16 ECMWF",
         'pointsize': '10pt'    }
 
